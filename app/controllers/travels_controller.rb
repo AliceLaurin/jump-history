2226853acd5a2508b1b06083b49ba4232d49d39d
catriona beamish <francois.lerosier@gmail.com>
2023-11-20 15:57:33 +0100

    Added

---
+++ app/controllers/travels_controller.rb
@@ -0,0 +1,26 @@
+class TravelsController < ApplicationController
+  def index
+    @travels = Travel.all
+  end
+
+  def new
+    @travel = Travel.new
+  end
+
+
+  def create
+    @travel = Travel.new(params_travel)
+    @travel.user_id = current_user.id
+    if @travel.save
+      redirect_to travel_path(@travel)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def params_travel
+    params.require(:travel).permit(:name, :picture, :address, :period, :perilousness, :description, :price, :duration, :number_of_travelers, :user_id)
+  end
+end
